aa87ab802ec43431cdbd37ebb9617c404ead319a
Luca Chmielarski <93C@pm.me>
2024-05-19 10:13:45 +0200

    chore: update proto of post-service

--- src/post-service/proto/post-service.pb.go
+++ src/post-service/proto/post-service.pb.go
@@ -20,7 +20,6 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-// ------------------ Messages ------------------ //
 type Location struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
