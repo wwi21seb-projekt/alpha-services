4e6a447ad78100f2e5da0c4c527fb0e566580b03
Luca Chmielarski <93c@pm.me>
2024-06-16 21:13:31 +0200

    fix(post): major refactoring of user-service, move from prometheus to otel

--- src/user-service/cmd/main.go
+++ src/user-service/cmd/main.go
@@ -4,20 +4,18 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"sync/atomic"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/wwi21seb-projekt/alpha-services/src/user-service/handler"
 	"github.com/wwi21seb-projekt/alpha-shared/config"
 	"github.com/wwi21seb-projekt/alpha-shared/db"
+	sharedGRPC "github.com/wwi21seb-projekt/alpha-shared/grpc"
 	sharedLogging "github.com/wwi21seb-projekt/alpha-shared/logging"
+	"github.com/wwi21seb-projekt/alpha-shared/metrics"
 	pbHealth "github.com/wwi21seb-projekt/alpha-shared/proto/health"
 	pbMail "github.com/wwi21seb-projekt/alpha-shared/proto/mail"
 	pbNotification "github.com/wwi21seb-projekt/alpha-shared/proto/notification"
 	pbUser "github.com/wwi21seb-projekt/alpha-shared/proto/user"
 	"github.com/wwi21seb-projekt/alpha-shared/tracing"
-	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
@@ -29,13 +27,8 @@ var (
 
 func main() {
 	// Initialize logger
-	logger := sharedLogging.GetZapLogger()
-	defer func(logger *zap.SugaredLogger) {
-		err := logger.Sync()
-		if err != nil {
-			logger.Fatal("Failed to sync logger", zap.Error(err))
-		}
-	}(logger)
+	logger, cleanup := sharedLogging.InitializeLogger(name)
+	defer cleanup()
 
 	// Load configuration
 	cfg, err := config.LoadConfig()
@@ -44,67 +37,38 @@ func main() {
 	}
 
 	// Initialize the database
-	database, err := db.NewDB(cfg.DatabaseConfig)
+	ctx := context.Background()
+	database, err := db.NewDB(ctx, cfg.DatabaseConfig)
 	if err != nil {
 		logger.Fatal("Failed to connect to the database", zap.Error(err))
 	}
 	defer database.Close()
 
-	// Create listener
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.Port))
-	if err != nil {
-		logger.Fatal("Failed to listen", zap.Error(err))
-	}
-
-	// Initialize prometheus metrics for gRPC server and client
-	reg := prometheus.NewRegistry()
-	srvMetrics := tracing.GetServerMetrics()
-	clMetrics := tracing.GetClientMetrics()
-	reg.MustRegister(srvMetrics)
-	reg.MustRegister(clMetrics)
-
-	// Setup metric for panic recoveries.
-	var panicCounter uint64 = 0
-	promauto.With(reg).NewCounterFunc(
-		prometheus.CounterOpts{
-			Name: "grpc_req_panics_recovered_total",
-			Help: "Total number of gRPC requests recovered from internal panic.",
-		},
-		func() float64 {
-			return float64(atomic.LoadUint64(&panicCounter))
-		},
-	)
-
-	// Init telemetry and create server
-	shutdown, err := tracing.InitTelemetry(context.Background(), name, version)
+	// Initialize tracing
+	tracingShutdown, err := tracing.InitializeTelemetry(ctx, name, version)
 	if err != nil {
 		logger.Fatal("Failed to initialize telemetry", zap.Error(err))
 	}
-	defer func() {
-		if err := shutdown(context.Background()); err != nil {
-			logger.Fatal("Failed to shutdown telemetry", zap.Error(err))
-		}
-	}()
-	otelgrpc.NewServerHandler()
+	defer tracingShutdown()
 
-	zapLogger := logger.With(zap.String("service", name)).Desugar()
-	grpcServer := grpc.NewServer(tracing.NewServerOptions(srvMetrics, zapLogger, &panicCounter)...)
-
-	// Create client connections
-	dialOpts := tracing.NewClientOptions(clMetrics, zapLogger)
-
-	mailConn, err := grpc.NewClient(cfg.ServiceEndpoints.MailServiceURL, dialOpts...)
+	// Intialize metrics
+	metricShutdown, err := metrics.InitializeMetrics(ctx, name, version)
 	if err != nil {
-		logger.Fatal("Failed to connect to mail service", zap.Error(err))
+		logger.Fatal("Failed to initialize metrics", zap.Error(err))
 	}
-	notificationConn, err := grpc.NewClient(cfg.ServiceEndpoints.NotificationServiceURL, dialOpts...)
-	if err != nil {
-		logger.Fatal("Failed to connect to notification service", zap.Error(err))
+	defer metricShutdown()
+
+	// Initialize gRPC client connections
+	if err = cfg.InitializeClients(logger.Desugar()); err != nil {
+		logger.Fatal("Failed to initialize gRPC clients", zap.Error(err))
 	}
 
 	// Create client stubs
-	mailClient := pbMail.NewMailServiceClient(mailConn)
-	notificationClient := pbNotification.NewNotificationServiceClient(notificationConn)
+	mailClient := pbMail.NewMailServiceClient(cfg.GRPCClients.MailService)
+	notificationClient := pbNotification.NewNotificationServiceClient(cfg.GRPCClients.NotificationService)
+
+	// Create the gRPC Server
+	grpcServer := grpc.NewServer(sharedGRPC.NewServerOptions(logger.Desugar())...)
 
 	// Register health service
 	pbHealth.RegisterHealthServer(grpcServer, handler.NewHealthServer())
@@ -114,20 +78,15 @@ func main() {
 	pbUser.RegisterSubscriptionServiceServer(grpcServer, handler.NewSubscriptionServer(logger, database, notificationClient))
 	pbUser.RegisterAuthenticationServiceServer(grpcServer, handler.NewAuthenticationServer(logger, database, mailClient))
 
-	// Initialize server metrics
-	srvMetrics.InitializeMetrics(grpcServer)
-
-	// Start metrics server
-	go func() {
-		err := tracing.StartMetricsServer(logger, reg)
-		if err != nil {
-			logger.Fatal("Failed to start metrics server", zap.Error(err))
-		}
-	}()
+	// Create listener
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.Port))
+	if err != nil {
+		logger.Fatal("Failed to listen", zap.Error(err))
+	}
 
 	// Start server
 	logger.Infof("Starting %s v%s on port %s", name, version, cfg.Port)
-	if err := grpcServer.Serve(lis); err != nil {
+	if err = grpcServer.Serve(lis); err != nil {
 		logger.Fatal("Failed to serve", zap.Error(err))
 	}
 }
